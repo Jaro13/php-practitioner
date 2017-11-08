f82472bd316a2f0fff3f2d8235b6860e34ec3601
rjarek <softart.poczta@gmail.com>
2017-11-08 11:12:48 +0100

    17

--- views/index.view.php
+++ views/index.view.php
@@ -1,4 +1,25 @@
 
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <title>This is Title!</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <nav>
+            
+            <li><a href="/">Home</a></li>
+            
+            <li><a href="/about">About page</a></li>
+            
+            <li><a href="/about/culture">About Culture</a></li>
+            
+            <li><a href="/contact">Contact us</a></li>
+            
+        </nav>
+    </body>
+</html>
 
 
 
